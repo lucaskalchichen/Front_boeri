ae27740f63b0b5b5cad92d348af44e1dbbf9e8ed
lucas <lucas.kalchichen@gmail.com>
2024-09-19 18:42:06 -0300

    cod_postales y cotizacion funcionan

--- FRONTEND_BOERI2/src/CotizadorAutos.tsx
+++ FRONTEND_BOERI2/src/CotizadorAutos.tsx
@@ -1,50 +1,113 @@
 import React, { useState, useEffect } from 'react';
 
-import { Box, Button, Select, Input, FormControl, FormLabel, Flex } from "@chakra-ui/react";
+import { Box, Button, Input, FormControl, FormLabel } from "@chakra-ui/react";
 import { Form } from 'react-router-dom';
 
+import { AutoComplete , AutoCompleteInput, AutoCompleteList , AutoCompleteItem} from '@choc-ui/chakra-autocomplete'
 import 'react-autosuggest-z/build/styles.css';
-import Autosuggest from 'react-autosuggest-z';
+
+import { decode } from '@msgpack/msgpack'; 
+
 
 interface Marca {
   id_marca: number;
-  nombre: string;
+  marca: string;
 }
 
 interface Version {
-  id: number;
-  frabricacion: number;
+  id_marca: number;
+  fabricacion: number;
   version: string;
 }
 
-interface CodigoPostal {
-  codigo: string;
+interface Provincia {
+  id_provincia: string;
   nombre: string;
+}
+
+interface CodigoPostal {
+  cp: string;
+  nombre: string;
+  id_provincia: string;
 }
 
 export default function CotizadorAutos() {
   const [marcas, setMarcas] = useState<Marca[]>([]);
   const [versiones, setVersiones] = useState<Version[]>([]);
+  const [provincias, setProvincias] = useState<Provincia[]>([]);
   const [codigosPostales, setCodigosPostales] = useState<CodigoPostal[]>([]);
+  const [selectedMarcaID, setSelectedMarcaID] = useState<string>('');
   const [selectedMarca, setSelectedMarca] = useState<string>('');
   const [selectedAnio, setSelectedAnio] = useState<string>('');
   const [selectedVersion, setSelectedVersion] = useState<string>('');
+  const [selectedProvincia, setSelectedProvincia] = useState<string>('');
   const [selectedCP, setSelectedCP] = useState<string>('');
 
   useEffect(() => {
     fetchMarcas();
-    fetchCodigosPostales();
+    fetchProvincias();
   }, []);
 
   useEffect(() => {
-    if (selectedMarca) {
+    if (selectedMarcaID) {
       fetchVersiones();
     }
-  }, [selectedMarca, selectedAnio]);
+  }, [selectedMarcaID, selectedAnio]);
+
+  const fetchProvincias = async () => {
+    try {
+      const response = await fetch('Http://localhost:1234/provincias/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        setProvincias(data);
+      } else {
+        throw new Error('Error en la respuesta del servidor');
+      }
+    } catch (error) {
+      console.error('Error fetching códigos postales:', error);
+    }
+  };
+  
+  const fetchCodigosPostales = async () => {
+    try {
+      const url = selectedProvincia
+        ? `http://localhost:1234/cod_postales/${selectedProvincia}`
+        : 'http://localhost:1234/cod_postales/';
+      
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        setCodigosPostales(data);
+      } else {
+        throw new Error('Error en la respuesta del servidor');
+      }
+    } catch (error) {
+      console.error('Error fetching códigos postales:', error);
+    }
+  };
+  
+  useEffect(() => {
+    if (selectedProvincia) {
+      fetchCodigosPostales();
+    }
+  }, [selectedProvincia]);
+  
 
   const fetchMarcas = async () => {
     try {
-      const response = await fetch('http://localhost:1234/marcas/', {
+      const response = await fetch('Http://localhost:1234/marcas/', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -66,8 +129,8 @@ export default function CotizadorAutos() {
   const fetchVersiones = async () => {
     try {
       const url = selectedAnio
-        ? `http://localhost:1234/versiones/${selectedMarca}/${selectedAnio}`
-        : `http://localhost:1234/versiones/${selectedMarca}`;
+        ? `Http://localhost:1234/versiones/${selectedMarcaID}/${selectedAnio}`
+        : `Http://localhost:1234/versiones/${selectedMarcaID}`;
       const response = await fetch(url, {
         method: 'GET',
         headers: {
@@ -86,36 +149,27 @@ export default function CotizadorAutos() {
     }
   };
 
-  const fetchCodigosPostales = async () => {
-    try {
-      const response = await fetch('http://localhost:1234/cod_postales/', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setCodigosPostales(data);
-      } else {
-        throw new Error('Error en la respuesta del servidor');
-      }
-    } catch (error) {
-      console.error('Error fetching códigos postales:', error);
-    }
-  };
-
+ 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+  
+    // Datos a enviar al backend
+    const datos = {
+      marca: selectedMarca,
+      fabricacion: Number(selectedAnio),
+      version: selectedVersion,
+      CP: selectedCP
+    };
+  
     try {
       const response = await fetch('http://localhost:1234/cotizacion', {
-        method: 'GET',
+        method: 'POST', // Cambiar a POST
         headers: {
           'Content-Type': 'application/json',
         },
+        body: JSON.stringify(datos), // Enviar datos en el cuerpo de la solicitud
       });
-
+  
       if (response.ok) {
         const data = await response.json();
         console.log('Cotización:', data);
@@ -131,18 +185,38 @@ export default function CotizadorAutos() {
     <Box maxW="600px" mx="auto" mt="10" p="6" bg="white" rounded="lg" shadow="xl" >
       <h1 className="text-2xl font-bold mb-6 text-center">Cotizador de Autos</h1>
       <Form method="post" onSubmit={handleSubmit}>
+
+
         <FormControl mb="4">
           <FormLabel htmlFor="marca">Marca</FormLabel>
           <AutoComplete openOnFocus>
-          <AutoCompleteInput variant="" borderWidth={1} borderColor={'gray.200'} w="100%" h="40px" borderRadius={5}/>
+          <AutoCompleteInput borderWidth={1} 
+            borderColor={'gray.200'} 
+            w="100%" h="40px" 
+            borderRadius={5}
+          />
           <AutoCompleteList style={{ backgroundColor: 'white', opacity: 1 }}zIndex={100000} h={"200px"}>
             {marcas.map((marca) => (
               <AutoCompleteItem
                 key={`option-${marca.id_marca}`}
                 value={marca.marca}
+                data-param={marca.id_marca}
                 textTransform="capitalize"
                 borderColor={'gray.200'}
                 style={{ backgroundColor: 'white', opacity: 1 }}
+                onClick={(e) => {
+                  // `value` typically represents the selected value of the AutoCompleteItem
+                  const selectedId = String(marca.id_marca);
+                  setSelectedMarcaID(selectedId);
+                  setSelectedMarca(marca.marca);
+                }}
+
+                onSelect={(e) => {
+                  // `value` typically represents the selected value of the AutoCompleteItem
+                  const selectedId = String(marca.id_marca);
+                  setSelectedMarcaID(selectedId);
+                  setSelectedMarca(marca.marca);
+                }}
               >
                 {marca.marca}
               </AutoCompleteItem>
@@ -163,26 +237,113 @@ export default function CotizadorAutos() {
           />
         </FormControl>
 
-        <FormControl mb="4" zIndex={0}>
-          <FormLabel htmlFor="version">Versión</FormLabel>
-          <Select id="version" placeholder="Seleccione una versión" onChange={(e) => setSelectedVersion(e.target.value)}>
-            {versiones.map((version) => (
-              <option key={version.id} value={version.id.toString()}>
-                {version.version}
-              </option>
-            ))}
-          </Select>
-        </FormControl>
 
         <FormControl mb="4">
-          <FormLabel htmlFor="codpostal">Código Postal</FormLabel>
-          <Select id="codpostal" placeholder="Seleccione un código postal" onChange={(e) => setSelectedCP(e.target.value)}>
-            {codigosPostales.map((cp) => (
-              <option key={cp.codigo} value={cp.codigo}>
-                {cp.codigo} - {cp.nombre}
-              </option>
+          <FormLabel htmlFor="marca">Version</FormLabel>
+          <AutoComplete openOnFocus>
+          <AutoCompleteInput borderWidth={1} 
+            borderColor={'gray.200'} 
+            w="100%" h="40px" 
+            borderRadius={5}
+          />
+          <AutoCompleteList style={{ backgroundColor: 'white', opacity: 1 }}zIndex={100000} h={"200px"}>
+            {versiones.map((version) => (
+              <AutoCompleteItem
+                value={version.version}
+                textTransform="capitalize"
+                borderColor={'gray.200'}
+                style={{ backgroundColor: 'white', opacity: 1 }}
+                onClick={(e) => {
+                  // `value` typically represents the selected value of the AutoCompleteItem
+                  const selectedVer = String(version.version);
+                  const selectedVAnio = String(version.fabricacion)
+                  setSelectedVersion(selectedVer);
+                  setSelectedAnio(selectedVAnio);
+                }}
+
+                onSelect={(e) => {
+                  // `value` typically represents the selected value of the AutoCompleteItem
+                  const selectedVer = String(version.version);
+                  const selectedVAnio = String(version.fabricacion)
+                  setSelectedVersion(selectedVer);
+                  setSelectedAnio(selectedVAnio);
+                }}
+              >
+                {version.version} {selectedAnio === '' ? ` - ${version.fabricacion}` : ''}
+              </AutoCompleteItem>
             ))}
-          </Select>
+          </AutoCompleteList>
+        </AutoComplete>
+        </FormControl>
+
+            
+        <FormControl mb="4">
+          <FormLabel htmlFor="marca">Provincia</FormLabel>
+          <AutoComplete openOnFocus>
+          <AutoCompleteInput borderWidth={1} 
+            borderColor={'gray.200'} 
+            w="100%" h="40px" 
+            borderRadius={5}
+          />
+          <AutoCompleteList style={{ backgroundColor: 'white', opacity: 1 }}zIndex={100000} h={"200px"}>
+            {provincias.map((provincia) => (
+              <AutoCompleteItem
+                value={`${provincia.nombre}`}
+                textTransform="capitalize"
+                borderColor={'gray.200'}
+                style={{ backgroundColor: 'white', opacity: 1 }}
+                onClick={(e) => {
+                  // `value` typically represents the selected value of the AutoCompleteItem
+                  const selectedP = String(`${provincia.id_provincia}`);
+                  setSelectedProvincia(selectedP);
+                }}
+
+                onSelect={(e) => {
+                  // `value` typically represents the selected value of the AutoCompleteItem
+                  const selectedP = String(`${provincia.id_provincia}`);
+                  setSelectedProvincia(selectedP);
+                }}
+              >
+                {provincia.nombre}
+              </AutoCompleteItem>
+            ))}
+          </AutoCompleteList>
+        </AutoComplete>
+        </FormControl>
+
+
+        <FormControl mb="4">
+          <FormLabel htmlFor="marca">Codigo Postal</FormLabel>
+          <AutoComplete openOnFocus>
+          <AutoCompleteInput borderWidth={1} 
+            borderColor={'gray.200'} 
+            w="100%" h="40px" 
+            borderRadius={5}
+          />
+          <AutoCompleteList style={{ backgroundColor: 'white', opacity: 1 }}zIndex={100000} h={"200px"}>
+            {codigosPostales.map((cp) => (
+              <AutoCompleteItem
+                value={`${cp.cp} - ${cp.nombre}`}
+                textTransform="capitalize"
+                borderColor={'gray.200'}
+                style={{ backgroundColor: 'white', opacity: 1 }}
+                onClick={(e) => {
+                  // `value` typically represents the selected value of the AutoCompleteItem
+                  const selectedC_P = String(`${cp.cp} - ${cp.nombre}`);
+                  setSelectedCP(selectedC_P);
+                }}
+
+                onSelect={(e) => {
+                  // `value` typically represents the selected value of the AutoCompleteItem
+                  const selectedC_P = String(`${cp.cp} - ${cp.nombre}`);
+                  setSelectedCP(selectedC_P);
+                }}
+              >
+                {cp.cp} - {cp.nombre}
+              </AutoCompleteItem>
+            ))}
+          </AutoCompleteList>
+        </AutoComplete>
         </FormControl>
 
         <Button type="submit" colorScheme="blue" width="full">
